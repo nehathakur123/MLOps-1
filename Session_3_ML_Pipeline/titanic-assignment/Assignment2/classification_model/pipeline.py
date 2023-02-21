d2cbebaa5e42a406bd62b0b0d96bd06aa00286ce
Neha Thakur <neha.thakur81@gmail.com>
2023-02-21 18:49:41 +0000

    test

--- Session_3_ML_Pipeline/titanic-assignment/Assignment2/classification_model/pipeline.py
+++ Session_3_ML_Pipeline/titanic-assignment/Assignment2/classification_model/pipeline.py
@@ -4,6 +4,7 @@ from feature_engine.imputation import (
     CategoricalImputer,
     MeanMedianImputer,
 )
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
